7c06a16dcae2a482acfff54b27ddeefbaabbce33
Keon <121371840+devkeon@users.noreply.github.com>
2024-07-14 22:58:22 +0900

    Add files via upload

---
+++ src/main/java/com/develop_mouse/gummy_dang/GummyDangApplication.java
@@ -0,0 +1,13 @@
+package com.develop_mouse.gummy_dang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GummyDangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GummyDangApplication.class, args);
+	}
+
+}
