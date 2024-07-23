b73c5384c315018c062e4fae3367bd37bad3e4a4
Keon <lune0410k@naver.com>
2024-07-23 19:03:49 +0900

    feat: create post entity Create post entity.

---
+++ src/main/java/com/develop_mouse/gummy_dang/post/domain/entity/Post.java
@@ -0,0 +1,39 @@
+package com.develop_mouse.gummy_dang.post.domain.entity;
+
+import com.develop_mouse.gummy_dang.member.domain.entity.Member;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Post {
+
+	@Id @Column(name = "post_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne
+	@JoinColumn(name = "member_id")
+	private Member member;
+
+	@NotNull
+	private String title;
+	private Integer likeCount;
+	private String description;
+
+}
