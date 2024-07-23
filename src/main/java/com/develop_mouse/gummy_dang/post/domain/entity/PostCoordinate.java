781b0039c167d1779931f134abd1caab13887bae
Keon <lune0410k@naver.com>
2024-07-23 19:22:16 +0900

    feat: post coordinate added and mapping with post

--- src/main/java/com/develop_mouse/gummy_dang/post/domain/entity/PostCoordinate.java
+++ src/main/java/com/develop_mouse/gummy_dang/post/domain/entity/PostCoordinate.java
@@ -1,9 +1,5 @@
 package com.develop_mouse.gummy_dang.post.domain.entity;
 
-import java.util.Set;
-
-import com.develop_mouse.gummy_dang.member.domain.entity.Member;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -11,7 +7,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -24,23 +19,18 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Post {
+public class PostCoordinate {
 
-	@Id @Column(name = "post_id")
+	@Id @Column(name = "post_coordinate_id")
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	@ManyToOne
-	@JoinColumn(name = "member_id")
-	private Member member;
-
-	@OneToMany(mappedBy = "post")
-	private Set<PostCoordinate> postCoordinates;
+	@JoinColumn(name = "post_id")
+	private Post post;
 
 	@NotNull
 	@Column(length = 50)
-	private String title;
-	private Integer likeCount;
-	private String description;
+	private String coordinate;
 
 }
