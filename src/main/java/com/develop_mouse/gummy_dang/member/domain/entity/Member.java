c4ae24b26ea46d2b74b53c4110f081403836cf27
Keon <lune0410k@naver.com>
2024-07-23 19:04:11 +0900

    feat: mapping post and member

--- src/main/java/com/develop_mouse/gummy_dang/member/domain/entity/Member.java
+++ src/main/java/com/develop_mouse/gummy_dang/member/domain/entity/Member.java
@@ -1,12 +1,16 @@
 package com.develop_mouse.gummy_dang.member.domain.entity;
 
+import java.util.Set;
+
 import com.develop_mouse.gummy_dang.common.entity.BaseEntity;
+import com.develop_mouse.gummy_dang.post.domain.entity.Post;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -22,6 +26,9 @@ public class Member extends BaseEntity {
 	@Id @Column(name = "member_id")
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	@OneToMany(mappedBy = "member")
+	private Set<Post> posts;
 
 	@NotNull
 	private String userName;
