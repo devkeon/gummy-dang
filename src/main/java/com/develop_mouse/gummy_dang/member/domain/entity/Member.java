7613e59272ad433a5e946e17b8dd0c6f312e1d56
Keon <lune0410k@naver.com>
2024-07-25 21:07:51 +0900

    feat: add role on member entity

--- src/main/java/com/develop_mouse/gummy_dang/member/domain/entity/Member.java
+++ src/main/java/com/develop_mouse/gummy_dang/member/domain/entity/Member.java
@@ -7,12 +7,15 @@ import org.hibernate.annotations.SQLRestriction;
 
 import com.develop_mouse.gummy_dang.common.entity.BaseEntity;
 import com.develop_mouse.gummy_dang.like.domain.entity.Like;
+import com.develop_mouse.gummy_dang.member.domain.Role;
 import com.develop_mouse.gummy_dang.post.domain.entity.Post;
 import com.develop_mouse.gummy_dang.walkrecord.domain.entity.WalkRecord;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -58,5 +61,8 @@ public class Member extends BaseEntity {
 	private String phoneNumber;
 	@Column(length = 20)
 	private String socialId;
+
+	@Enumerated(value = EnumType.STRING)
+	private Role role;
 
 }
