386532916fcc5d74b07faeea1a26b1c4f5ba3ff9
Keon <lune0410k@naver.com>
2024-07-27 18:25:40 +0900

    feat: add member service

---
+++ src/main/java/com/develop_mouse/gummy_dang/member/service/MemberServiceImpl.java
@@ -0,0 +1,44 @@
+package com.develop_mouse.gummy_dang.member.service;
+
+import java.util.UUID;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.develop_mouse.gummy_dang.common.domain.response.Response;
+import com.develop_mouse.gummy_dang.member.domain.Role;
+import com.develop_mouse.gummy_dang.member.domain.entity.Member;
+import com.develop_mouse.gummy_dang.member.domain.request.SignUpRequest;
+import com.develop_mouse.gummy_dang.member.repository.MemberRepository;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+@Transactional
+@RequiredArgsConstructor
+public class MemberServiceImpl implements MemberService {
+
+	private final MemberRepository memberRepository;
+	private final PasswordEncoder passwordEncoder;
+
+	@Override
+	public Response<Void> singUp(SignUpRequest signUpRequest) {
+
+		Member signUpMember = Member.builder()
+			.userName(signUpRequest.getUserName())
+			.password(passwordEncoder.encode(signUpRequest.getPassword()))
+			.nickname(signUpRequest.getNickname() == null ?
+				"임시" + UUID.randomUUID().toString().substring(1, 9)
+				: signUpRequest.getNickname())
+			.role(Role.MEMBER)
+			.build();
+
+		memberRepository.save(signUpMember);
+
+		return Response.ok();
+	}
+
+}
