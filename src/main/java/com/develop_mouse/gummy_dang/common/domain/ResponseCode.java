5e9ce697d83d6026cf7e3a36944ea46727e845b0
Keon <lune0410k@naver.com>
2024-07-28 02:15:48 +0900

    feat: add message on response code

--- src/main/java/com/develop_mouse/gummy_dang/common/domain/ResponseCode.java
+++ src/main/java/com/develop_mouse/gummy_dang/common/domain/ResponseCode.java
@@ -6,8 +6,11 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum ResponseCode {
-	OK("COM-000"), MEMBER_NOT_FOUND("MEM-001"), MEMBER_USERNAME_DUPLICATION("MEM-000");
+	OK("COM-000", "Ok."),
+	MEMBER_NOT_FOUND("MEM-001", "No such member exist."),
+	MEMBER_USERNAME_DUPLICATION("MEM-000", "Username already exists.");
 
 	private final String responseCode;
+	private final String message;
 
 }
