b263572e25264a313f542a1f5ca768ec10fac400
Keon <lune0410k@naver.com>
2024-07-28 02:15:19 +0900

    feat: add Global Exception and Business Exception

---
+++ src/main/java/com/develop_mouse/gummy_dang/common/Exception/BusinessException.java
@@ -0,0 +1,15 @@
+package com.develop_mouse.gummy_dang.common.Exception;
+
+import com.develop_mouse.gummy_dang.common.domain.ResponseCode;
+
+import lombok.Getter;
+
+@Getter
+public class BusinessException extends RuntimeException{
+
+	private ResponseCode responseCode;
+
+	public BusinessException(ResponseCode responseCode) {
+		super(responseCode.getMessage());
+	}
+}
