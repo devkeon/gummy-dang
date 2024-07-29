d275b5870a571f2a23b6a5411a7a2250f401088f
Keon <lune0410k@naver.com>
2024-07-29 21:40:11 +0900

    fix: fix gummy repository domain

--- src/main/java/com/develop_mouse/gummy_dang/walkrecord/repository/GummyRepository.java
+++ src/main/java/com/develop_mouse/gummy_dang/walkrecord/repository/GummyRepository.java
@@ -2,5 +2,7 @@ package com.develop_mouse.gummy_dang.walkrecord.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface GummyRepository extends JpaRepository<GummyRepository, Long> {
+import com.develop_mouse.gummy_dang.walkrecord.domain.entity.Gummy;
+
+public interface GummyRepository extends JpaRepository<Gummy, Long> {
 }
