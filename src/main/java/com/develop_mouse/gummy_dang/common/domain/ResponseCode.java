6ea9b85f919ce896d88b2ac1adc47c47692952a0
Keon <lune0410k@naver.com>
2024-07-30 23:58:13 +0900

    feat: add new response code and edit business exception

--- src/main/java/com/develop_mouse/gummy_dang/common/domain/ResponseCode.java
+++ src/main/java/com/develop_mouse/gummy_dang/common/domain/ResponseCode.java
@@ -8,7 +8,11 @@ import lombok.RequiredArgsConstructor;
 public enum ResponseCode {
 	OK("COM-000", "Ok."),
 	MEMBER_NOT_FOUND("MEM-001", "No such member exist."),
-	MEMBER_USERNAME_DUPLICATION("MEM-000", "Username already exists.");
+	MEMBER_USERNAME_DUPLICATION("MEM-000", "Username already exists."),
+	MEMBER_UNAUTHORIZED("MEM-002", "Member not matched."),
+	WALK_RECORD_NOT_FOUND("WRC-001", "No such walk record exists."),
+	GUMMY_IMG_NOT_FOUND("GUM-001", "No such gummy exists.")
+	;
 
 	private final String responseCode;
 	private final String message;
