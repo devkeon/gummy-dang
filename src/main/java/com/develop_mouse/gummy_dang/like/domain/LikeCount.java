3d6e1b5bc30d2dbe39b49c6165136092743d5074
Keon <lune0410k@naver.com>
2024-11-22 01:59:46 +0900

    feat: add like dto

---
+++ src/main/java/com/develop_mouse/gummy_dang/like/domain/LikeCount.java
@@ -0,0 +1,14 @@
+package com.develop_mouse.gummy_dang.like.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class LikeCount {
+
+	private Integer likeCount;
+
+}
---
+++ src/main/java/com/develop_mouse/gummy_dang/like/domain/LikeResponse.java
@@ -0,0 +1,15 @@
+package com.develop_mouse.gummy_dang.like.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class LikeResponse {
+
+	private Long likeId;
+	private Long postId;
+
+}
